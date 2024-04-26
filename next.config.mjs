f091bf98c393c6d2d3f9a2bfc0cfbbed9ab86458
Robin.Wang <3Jg$94sbQ@q1>
2024-04-26 15:27:09 +0800

    fix(cv): add .wasm webpack config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.wasm$/i,
+      type: "javascript/auto",
+      use: [
+        {
+          loader: "file-loader",
+        },
+      ],
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;

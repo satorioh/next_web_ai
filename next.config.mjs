59386963165956f5f5db18320977bb05fe3b86a4
Robin.Wang <3Jg$94sbQ@q1>
2024-04-26 17:34:09 +0800

    fix(cv): try file-loader

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // webpack: (config, options) => {
-  //   config.module.rules.push({
-  //     test: /\.wasm$/i,
-  //     type: "javascript/auto",
-  //     use: [
-  //       {
-  //         loader: "file-loader",
-  //       },
-  //     ],
-  //   });
-  //
-  //   return config;
-  // },
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.wasm$/i,
+      type: "javascript/auto",
+      use: [
+        {
+          loader: "file-loader",
+        },
+      ],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;

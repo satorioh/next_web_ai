83fa7145fafc4497e0676beb08fd095bcafd025a
Robin.Wang <3Jg$94sbQ@q1>
2024-04-28 16:35:01 +0800

    feat(worker): add IndexedDB cache for model

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,2 @@
 export const SITE_TITLE = "Next Web AI";
+export const BACKEND_URL_PREFIX = "https://api.regulusai.top:8443/api/v1/webml/";
--- worker/rps.worker.ts
+++ worker/rps.worker.ts
@@ -1,13 +1,37 @@
 "use client";
 
 import * as tf from "@tensorflow/tfjs";
-import { setWasmPaths, getThreadsCount } from "@tensorflow/tfjs-backend-wasm";
+// import { setWasmPaths, getThreadsCount } from "@tensorflow/tfjs-backend-wasm";
 import "@tensorflow/tfjs-backend-webgpu";
 import "@tensorflow/tfjs-backend-webgl";
+import { BACKEND_URL_PREFIX } from "@/lib/constants";
 
 // let device = "wasm";
 let device = "webgl";
 let model: null | tf.GraphModel = null;
+const IDB_URL = "indexeddb://rps-model";
+const modelName = "rps";
+
+async function getModelLastUpdateTime() {
+  try {
+    const res = await fetch(
+      `${BACKEND_URL_PREFIX}getModelLastUpdateTime?modelName=${modelName}`,
+    );
+    const { lastUpdateTime } = await res.json();
+    return new Date(lastUpdateTime);
+  } catch (e) {
+    console.log("getModelLastUpdateTime Error", e);
+  }
+}
+
+async function isModelLatest(dateSaved: Date) {
+  const lastUpdateTime = await getModelLastUpdateTime();
+  if (!lastUpdateTime) return true;
+  console.log(
+    `model lastUpdateTime: ${lastUpdateTime.toLocaleString()}, savedTime: ${dateSaved.toLocaleString()}`,
+  );
+  return dateSaved.getTime() >= lastUpdateTime.getTime();
+}
 
 async function init() {
   console.log("init");
@@ -32,24 +56,51 @@ async function load_model() {
   } catch (e) {
     console.log("setBackend Error", e);
   }
+
+  // 检测indexeddb是否有模型，如果有则直接加载；如果没有则下载模型并保存到indexeddb
+  const modelInfo = await tf.io.listModels();
+  console.log("modelInfo", modelInfo);
+  if (modelInfo[IDB_URL]) {
+    const dateSaved = modelInfo[IDB_URL].dateSaved;
+    const isLatest = await isModelLatest(dateSaved);
+    if (!isLatest) {
+      // 清空indexeddb中的模型database
+      console.log("model not latest, remove model...");
+      await tf.io.removeModel(IDB_URL);
+      console.log("model not latest, downloading...");
+      await downloadModel();
+    } else {
+      console.log("model exists and latest");
+      model = await tf.loadGraphModel(IDB_URL);
+      postMessage({ type: "modelLoading", progress: 100 });
+    }
+  } else {
+    console.log("model not exists, downloading...");
+    await downloadModel();
+  }
+  console.log("model loaded", model);
+  let threads = 0;
+  // if (device === "wasm") {
+  //   try {
+  //     threads = getThreadsCount();
+  //   } catch (e) {
+  //     console.log("getThreadsCount Error", e);
+  //   }
+  // }
+  postMessage({ type: "modelLoaded", threads, deviceName: device });
+}
+
+async function downloadModel() {
   model = await tf.loadGraphModel(
-    "https://regulussig.s3.ap-southeast-1.amazonaws.com/tfjs/model/model.json",
+    "https://regulussig.s3.ap-southeast-1.amazonaws.com/tfjs/model/rps/model.json",
     {
       onProgress: (fractions) => {
         postMessage({ type: "modelLoading", progress: fractions * 100 });
       },
     },
   );
-  console.log("model loaded", model);
-  let threads = 0;
-  if (device === "wasm") {
-    try {
-      threads = getThreadsCount();
-    } catch (e) {
-      console.log("getThreadsCount Error", e);
-    }
-  }
-  postMessage({ type: "modelLoaded", threads, deviceName: device });
+  const savedResult = await model.save(IDB_URL);
+  console.log("model saved to db", savedResult);
 }
 
 async function run_model(input: ImageData) {

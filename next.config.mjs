cac3f31e72529af1f848f4555e1ed625d722691c
Robin.Wang <3Jg$94sbQ@q1>
2024-04-28 09:22:27 +0800

    build(webpack): remove webpack custom config

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.wasm$/i,
-      type: "javascript/auto",
-      use: [
-        {
-          loader: "file-loader",
-        },
-      ],
-    });
-
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;

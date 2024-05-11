7af9a96c9c6f3f626a15a032ad8f98f4bfcda4ba
Robin.Wang <3Jg$94sbQ@q1>
2024-05-11 17:16:37 +0800

    build(project): add env file

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,6 @@
+const isProd = process.env.NEXT_PUBLIC_ENV === "production";
+
 export const SITE_TITLE = "Next Web ML";
-export const BACKEND_URL_PREFIX = "https://api.regulusai.top:2096/api/v1/webml/";
-// export const BACKEND_URL_PREFIX = "http://0.0.0.0:8000/api/v1/webml/";
+export const BACKEND_URL_PREFIX = isProd
+  ? "https://api.regulusai.top:2096/api/v1/webml/"
+  : "http://0.0.0.0:8000/api/v1/webml/";
